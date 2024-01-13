bcd5aabbc2f3213779f5161e7447d8955ae56a78
Its_Shojib <mdshojib922@gmail.com>
2024-01-14 01:24:37 +0600

    Chat box added

--- src/Pages/Home/Chat.jsx
+++ src/Pages/Home/Chat.jsx
@@ -1,9 +1,17 @@
-
-
+import { BsCameraVideoFill } from "react-icons/bs";
+import { IoIosPersonAdd } from "react-icons/io";
+import { CiCircleMore } from "react-icons/ci";
 const Chat = () => {
     return (
-        <div className="text-left">
-            <p> Hello, I am Chat </p>
+        <div className="">
+            <div className="h-12 flex justify-between items-center bg-[#5d5b8d] p-2 text-gray-400">
+                <span className="text-black font-bold">Jane</span>
+                <div className="flex gap-2 cursor-pointer">
+                    <BsCameraVideoFill className='text-2xl' />
+                    <IoIosPersonAdd className='text-2xl' />
+                    <CiCircleMore className='text-2xl' />
+                </div>
+            </div>
         </div>
     )
 }
